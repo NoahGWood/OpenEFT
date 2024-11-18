6889023a835cd69657280be38a16527f079b30f3
Kourosh Mobl <moblkourosh@gmail.com>
2024-11-17 20:36:38 -0600

    Update openeft.py

--- openeft.py
+++ openeft.py
@@ -21,21 +21,21 @@ def main(a):
         ) from exc
     execute_from_command_line(a)
     
-def browser():
-    time.sleep(3) # Give some time for django to fully load.
-    print("RUNNING BROWSER")
-    if 'nt' in os.name:
-        if check_output("wsl ./browser/windows/chrome.exe --app=http://localhost:8080", shell=True) is not None:
-            pass
-    elif 'posix' in os.name or 'linux' in os.name:
-        if check_output("./browser/linux/chrome.AppImage --app=http://localhost:8080/", shell=True) is not None:
-            pass
-    # Force exit if not already
-    x.cleanup()
-    os._exit(1)
+# def browser():
+#     time.sleep(3) # Give some time for django to fully load.
+#     print("RUNNING BROWSER")
+#     if 'nt' in os.name:
+#         if check_output("wsl ./browser/windows/chrome.exe --app=http://localhost:8080", shell=True) is not None:
+#             pass
+#     elif 'posix' in os.name or 'linux' in os.name:
+#         if check_output("./browser/linux/chrome.AppImage --app=http://localhost:8080/", shell=True) is not None:
+#             pass
+#     # Force exit if not already
+#     x.cleanup()
+#     os._exit(1)
     
 if __name__ == '__main__':
-    br = threading.Thread(target=browser, args=())
-    br.start()
+    # br = threading.Thread(target=browser, args=())
+    # br.start()
     main(["openeft.py", "runserver", "0.0.0.0:8080","--noreload"])
     x.cleanup()
